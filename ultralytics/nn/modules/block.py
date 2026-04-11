109748a5d53c9dfd6e1ee269f7c6c485ae2709f2
1980243524 <1980243524@qq.com>
2026-04-11 15:09:40 +0800

    feat ema

--- ultralytics/nn/modules/block.py
+++ ultralytics/nn/modules/block.py
@@ -2318,3 +2318,47 @@ class RealNVP(nn.Module):
             self.float()
         z, log_det = self.backward_p(x)
         return self.prior.log_prob(z) + log_det
+
+
+class EMA(nn.Module):
+    """Efficient Multi-Scale Attention Module."""
+    def __init__(self, c1, c2, factor=8):
+        super(EMA, self).__init__()
+        self.groups = max(1, c1 // factor)
+        self.conv_match = nn.Identity() if c1 == c2 else Conv(c1, c2, 1, 1)
+        assert c2 % self.groups == 0, f"Channel size {c2} should be divisible by groups {self.groups}"
+        
+        self.softmax = nn.Softmax(-1)
+        self.agp = nn.AdaptiveAvgPool2d((1, 1))
+        self.pool_h = nn.AdaptiveAvgPool2d((None, 1))
+        self.pool_w = nn.AdaptiveAvgPool2d((1, None))
+        self.gn = nn.GroupNorm(c2 // self.groups, c2 // self.groups)
+        self.conv1x1 = nn.Conv2d(c2 // self.groups, c2 // self.groups, kernel_size=1, stride=1, padding=0)
+        self.conv3x3 = nn.Conv2d(c2 // self.groups, c2 // self.groups, kernel_size=3, stride=1, padding=1)
+
+    def forward(self, x):
+        x = self.conv_match(x)
+        b, c, h, w = x.size()
+        group_x = x.reshape(b * self.groups, -1, h, w)
+        x_h = self.pool_h(group_x)
+        x_w = self.pool_w(group_x).permute(0, 1, 3, 2)
+        hw = self.conv1x1(torch.cat([x_h, x_w], dim=2))
+        x_h, x_w = torch.split(hw, [h, w], dim=2)
+        x1 = self.gn(group_x * x_h.sigmoid() * x_w.permute(0, 1, 3, 2).sigmoid())
+        x2 = self.conv3x3(group_x)
+        x11 = self.softmax(self.agp(x1).reshape(b * self.groups, -1, 1).permute(0, 2, 1))
+        x12 = x2.reshape(b * self.groups, c // self.groups, -1)
+        x21 = self.softmax(self.agp(x2).reshape(b * self.groups, -1, 1).permute(0, 2, 1))
+        x22 = x1.reshape(b * self.groups, c // self.groups, -1)
+        weights = (torch.matmul(x11, x12) + torch.matmul(x21, x22)).reshape(b * self.groups, 1, h, w)
+        return (group_x * weights.sigmoid()).reshape(b, c, h, w)
+
+
+class RepC3_EMA(RepC3):
+    """Rep C3 with EMA attention."""
+    def __init__(self, c1, c2, n=3, e=1.0):
+        super().__init__(c1, c2, n, e)
+        self.ema = EMA(c2, c2)
+
+    def forward(self, x):
+        return self.ema(super().forward(x))
